a95ce7a32ce62a1c0bd8aaa4822825e378406d01
XXXYJade17 <17276090504@139.com>
2025-10-18 16:39:42 +0800

    Init

--- database/create.sql
+++ database/create.sql
@@ -1,58 +1,118 @@
 create database hiphop_ghetto;
 use hiphop_ghetto;
 # 用户表
-CREATE TABLE user (
-                      `id` bigint(20) NOT NULL COMMENT '用户ID（雪花算法）',
-                      `username` varchar(20) DEFAULT NULL COMMENT '用户名',
-                      `name` varchar(10) DEFAULT NULL COMMENT '姓名',
-                      `password` varchar(64) NOT NULL COMMENT '密码',
-                      `phone` varchar(20) DEFAULT NULL COMMENT '手机号',
-                      `sex` tinyint(1) DEFAULT 0 COMMENT '性别（0-未知，1-男，2-女）',
-                      `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
-                      `create_time` datetime NOT NULL COMMENT '创建时间',
-                      `update_time` datetime NOT NULL COMMENT '修改时间',
-                      `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '数据状态（0-正常，1-已删除）',
-                      PRIMARY KEY (`id`),
-                      index `idx_username` (`username`),
-                      index `idx_phone` (`phone`),
-                      index `idx_status` (`status`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+create table user (
+                      id bigint(20) primary key , # 用户ID（主键）
+                      username varchar(20) not null unique , # 用户名
+                      password varchar(100) not null , # 密码
+                      name varchar(20) default null unique , # 姓名
+                      id_card varchar(18) default null unique , # 身份证号
+                      phone varchar(20) default null unique , # 手机号
+                      sex tinyint(1) default 0 , # 性别（0-未知，1-男，2-女）
+                      avatar varchar(255) default null , # 头像url
+                      create_time datetime not null , # 创建时间
+                      update_time datetime not null , # 修改时间
+                      status tinyint(1) NOT NULL DEFAULT 0 #数据状态（0-正常，1-已删除）
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 # 评分表
 create table score (
-                       id bigint(20) comment '评分Id',
-                       user_id bigint(20) not null comment '发起用户Id',
-                       title varchar(20) not null comment '标题',
-                       description text comment '描述',
-                       start_time datetime comment '创建时间',
-                       status tinyint(1) NOT NULL DEFAULT 0 COMMENT '数据状态（0-正常，1-已删除）',
-                       primary key (`id`),
-                       index idx_user_id (user_id)
-) engine =InnoDB default charset =utf8mb4 comment '评分活动表';
-# 评分图片
+                       id bigint(20) primary key , # 评分Id（主键）
+                       user_id bigint(20) not null , # 发起用户Id（外键）
+                       title varchar(20) not null , # 标题
+                       description text , # 描述
+                       views bigint(10) default 0 ,
+                       create_time datetime not null , # 创建时间
+                       update_time datetime not null , # 修改时间
+                       status tinyint(1) NOT NULL DEFAULT 0 , # 数据状态（0-正常，1-已删除）
+                       constraint fk_score_user_id foreign key (user_id)
+                           references user(id)
+) engine =InnoDB default charset =utf8mb4;
+# 评分图片表
 create table score_image (
-                             id bigint auto_increment primary key comment '图片Id',
-                             score_id bigint(20) comment '评分Id',
-                             url varchar(255) DEFAULT NULL COMMENT '图片URL',
-                             constraint fk_activity_image foreign key (score_id)
+                             id bigint auto_increment primary key , # 图片Id（主键）
+                             score_id bigint(20) not null , # 评分Id（外键）
+                             url varchar(255) not null , # 图片URL
+                             constraint fk_score_image_score_id foreign key (score_id)
                                  references score(id)
-                                 on delete cascade,
-                             index idx_activity_id (score_id)
-) engine =InnoDB default charset =utf8mb4 comment '评分图片表';
-# 评分详情
+) engine =InnoDB default charset =utf8mb4;
+# 评分详情表
 create table score_detail (
-                              score_id bigint(20) comment '活动Id',
-                              one bigint default 0 comment '1分',
-                              two bigint default 0 comment '2分',
-                              three bigint default 0 comment '3分',
-                              four bigint default 0 comment '4分',
-                              five bigint default 0 comment '5分',
-                              six bigint default 0 comment '6分',
-                              seven bigint default 0 comment '7分',
-                              eight bigint default 0 comment '8分',
-                              nine bigint default 0 comment '9分',
-                              ten bigint default 0 comment '10分',
-                              primary key (score_id),
-                              constraint fk_activity_id foreign key (score_id)
+                              score_id bigint(20) primary key, # 活动Id（主键）
+                              one bigint(10) default 0 , # 1分
+                              two bigint(10) default 0 , # 2分
+                              three bigint(10) default 0 , # 3分
+                              four bigint(10) default 0 , # 4分
+                              five bigint(10) default 0 , # 5分
+                              six bigint(10) default 0 , # 6分
+                              seven bigint(10) default 0 , # 7分
+                              eight bigint(10) default 0 , # 8分
+                              nine bigint(10) default 0 , # 9分
+                              ten bigint(10) default 0 , # 10分
+                              constraint fk_score_detail_score_id foreign key (score_id)
                                   references score(id)
-                                  on delete cascade
-) engine =InnoDB default charset =utf8mb4 comment '评分详情表';
+) engine =InnoDB default charset =utf8mb4;
+# 帖子表
+create table posting (
+                         id bigint(20) primary key , # 帖子ID（主键）
+                         user_id bigint(20) not null , # 发起用户Id（外键）
+                         title varchar(20) not null , # 标题
+                         content text , # 内容
+                         view bigint(10) default 0 , # 浏览量
+                         `like` bigint(10) default 0 , # 点赞
+                         create_time datetime not null , # 创建时间
+                         update_time datetime not null , # 修改时间
+                         status tinyint(1) NOT NULL DEFAULT 0 , # 数据状态（0-正常，1-已删除）
+                         constraint fk_posting_user_id foreign key (user_id)
+                             references user(id)
+) engine =InnoDB default charset =utf8mb4;
+# 帖子图片表
+create table posting_image (
+                               id bigint auto_increment primary key , # 图片Id（主键）
+                               posting_id bigint(20) not null , # 帖子Id（外键）
+                               url varchar(255) not null , # 图片URL
+                               constraint fk_posting_image_posting_id foreign key (posting_id)
+                                   references posting(id)
+) engine =InnoDB default charset =utf8mb4;
+# 帖子评论表
+create table posting_comment (
+                                 id bigint(20) primary key , # 评论ID（主键）
+                                 posting_id bigint(20) not null , # 帖子ID（外键）
+                                 user_id bigint(20) not null , # 评论用户ID（外键）
+                                 content text,
+                                 create_time datetime not null , # 创建时间
+                                 update_time datetime not null , # 修改时间
+                                 status tinyint(1) NOT NULL DEFAULT 0, # 数据状态（0-正常，1-已删除）
+                                 constraint fk_posting_comment_posting_id foreign key (posting_id)
+                                     references posting(id),
+                                 constraint fk_posting_comment_user_id foreign key (user_id)
+                                     references user(id)
+) engine =InnoDB default charset =utf8mb4;
+# 帖子点赞记录表
+create table posting_like (
+                              id bigint auto_increment primary key , # 点赞记录ID
+                              posting_id bigint(20) not null , # 帖子ID（外键）
+                              user_id bigint(20) not null , # 点赞用户ID
+                              create_time datetime not null , # 创建时间
+                              update_time datetime not null , # 修改时间
+                              status tinyint(1) NOT NULL DEFAULT 0 , # 数据状态（0-正常，1-已删除）
+                              constraint fk_posting_like_posting_id foreign key (posting_id)
+                                  references posting(id),
+                              constraint fk_posting_like_user_id foreign key (user_id)
+                                  references user(id)
+) engine =InnoDB default charset =utf8mb4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
